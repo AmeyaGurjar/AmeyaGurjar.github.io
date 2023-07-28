98eaff52d1469ad7d9693093e37d6c14fd3b3ac8
AmeyaGurjar <amey992004@gmail.com>
2023-07-29 02:15:40 +0530

    Meow

---
+++ js/donate.js
@@ -0,0 +1,5 @@
+const btnResponse = {"paypalbtn":"https://paypal.me/AmeyMeow", "gitbtn":"https://github.com/sponsors/AmeyaGurjar", "buymecoffeebtn":"https://www.buymeacoffee.com/amey992004", "kofibtn":"https://ko-fi.com/ameyagurjar"};
+
+const btnClick = (btn) => {
+    window.location = btnResponse[btn.id];
+};
